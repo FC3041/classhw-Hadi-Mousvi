2f101eff5976140e9725be07b3ab2b53b6784997
Hadi Mousavi <mohammd_mousavi@comp.iust.ac.ir>
2025-03-09 09:27:48 +0330

    S08 statics

---
+++ S08/Class/S8con.Test/Test1.cs
@@ -0,0 +1,22 @@
+﻿namespace S8con.Test;
+
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using S8con;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        int i = 1;
+        Assert.AreEqual(i+1 , 2);
+    }
+
+    public void TestMethod2()
+    {
+        int result = S8con.Program.add(1,2);
+        Assert.AreEqual(result , 3);    
+    }
+
+}
---
+++ S08/Class/S8con/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace S8con;
+
+public class Program
+{
+
+    public static void reverse(string s , out string srev)
+    {
+        srev = "";
+        foreach(char c in s)
+        {
+            srev = c + srev;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string name = "Hadi";
+        string srev;
+        reverse(name , out srev);
+        Console.WriteLine(srev);
+    }
+
+    static void Main1(string[] args)
+    {
+        int sum = 0;
+        while(true)
+        {
+            Console.WriteLine("Nums ?");
+            string s = Console.ReadLine();
+            if(string.IsNullOrEmpty(s))
+                break;
+            int n;
+            if(! int.TryParse(s, out n))
+            {
+                Console.WriteLine("Wrong! Try again.");
+                continue;
+            }
+            sum += n;
+                
+            string sout;
+            prepend_user(s ,out sout);
+            Console.WriteLine(sout);
+        }
+        Console.WriteLine(sum);
+        // for(int i=0 ; i < args.Length ; i++)
+        //     Console.WriteLine($"args{i} : {args[i]}");
+    }
+
+    public static void prepend_user(string s , out string sout)
+    {
+        sout = "user: "+s;
+    }
+
+    public static int add(int a , int b)
+    {
+        return a+b;
+    }
+}
