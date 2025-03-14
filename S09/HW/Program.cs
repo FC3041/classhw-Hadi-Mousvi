327efd38ecd64b00d62c88e71518b15f3bfdd26d
Hadi Mousavi <mohammd_mousavi@comp.iust.ac.ir>
2025-03-14 11:38:17 +0330

    Registering students with file

---
+++ S09/HW/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HW;
+
+class Program
+{
+    public static void RegisterStds(string filename)
+    {
+       string[] rawstd =  File.ReadAllLines(filename);
+       Student[] students = new Student[rawstd.Length];
+       int i = 0;
+       foreach(string s in rawstd)
+       {
+            students[i] = Student.Parse(s);
+            i++;
+       }
+    }
+
+    static void Main(string[] args)
+    {
+
+    }
+}
